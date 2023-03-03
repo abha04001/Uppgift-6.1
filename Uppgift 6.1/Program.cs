77739018abc8a77a16e08237d42ae84935bc6865
Elev <Elev@TEB125103>
2023-03-03 14:23:44 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift_6._1
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Addera (2, 3, 5);
+            Console.WriteLine("summan är " + Addera(2, 3, 5));
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+           
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
